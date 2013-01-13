e51dfd26ec2dc5bb5aeba23112eca3cbef51ee61
Anders Olme <anders.olme@gmail.com>
2013-01-13 18:11:50 +0100

    some initial parsing and gui test

--- ljudman.py
+++ ljudman.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 *-*
 import subprocess
 import sys
+import re
 
-def get_sinks():
+
+def parse_sink_inputs(sink_inputs):
+
+    indexfinder = re.compile(r'(?P<index>\s*index\:\s*(\d*))', re.M)
+    appnamefinder = re .compile(r'(?:\s*application\.name\s*=\s*\"(.*)")', re.M)
+    sinks = []
+
+    for match in indexfinder.finditer(sink_inputs):
+        appindex = match.group(2)
+        start = match.span()[1]
+        appname = appnamefinder.search(sink_inputs[start:])
+        appname = appname.group(1)
+        sinks.append({'index': appindex, 'appname': appname})
+
+        print {'index': appindex, 'appname': appname}
+    return sinks
+
+
+def get_sink_inputs():
+
     cmd = "pacmd"
-    paramter = "list-sinks"
+    paramter = "list-sink-inputs"
 
     try:
-        result = subprocess.check_output([cmd, paramter]).splitlines()
+        result = subprocess.check_output([cmd, paramter])
+        parse_sink_inputs(result)
 
     except OSError as e:
         print >>sys.stderr, "Execution failed:", e
+
+get_sink_inputs()
